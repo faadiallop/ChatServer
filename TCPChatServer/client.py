3e3f910f499663b816f6793b51cda849a906b8f3
Faa Diallo <faadiallop@gmail.com>
2023-09-16 23:37:26 -0700

    Allowed multiple clients to connect to the server & send a single msg each.

---
+++ TCPChatServer/client.py
@@ -0,0 +1,16 @@
+#Created: 09/14/2023
+#Creator: Faa Diallo
+#Simple client to connect the TCP server and send a message to it.
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    msg = bytes(input("What message would you like to send the server?\n"), "utf-8")
+    s.sendall(msg)
+    data = s.recv(1024)
+
+print(f"Received \"{data.decode('utf-8')}\"")
+
---
+++ TCPChatServer/server.py
@@ -0,0 +1,29 @@
+#Created 09/14/2023
+#Creator: Faa Diallo
+#This is a TCP server that receives a message and echos it back to
+#ths client. After that it closes the connection.
+import socket
+
+HOST = ""
+PORT = 65432
+
+def bytesToStr(bytes_data):
+    return bytes_data.decode('utf-8')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    print(f"Login to the server on {PORT}.")
+    s.listen()
+    conn1, addr1 = s.accept()
+    print(f"Connected by {addr1}")
+    with conn1:
+        s.listen()
+        conn2, addr2 = s.accept()
+        print(f"Connected by {addr2}")
+        with conn2:
+            while True:
+                data1 = conn1.recv(1_000_000)
+                print(bytesToStr(data1))
+                data2 = conn2.recv(1_000_000)
+                print(bytesToStr(data2))
+            
