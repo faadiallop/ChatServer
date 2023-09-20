e7e8f7086097b4aabde9002a1b5b54379f74139e
Faa Diallo <faadiallop@gmail.com>
2023-09-20 13:31:07 -0700

    Cleaned up code and formatting

--- TCPChatServer/client.py
+++ TCPChatServer/client.py
@@ -6,12 +6,34 @@ import socket
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
-    while True:
-        msg = bytes(input("What message would you like to send the server?\n"), "utf-8")
-        s.sendall(msg)
-        data = s.recv(1024)
+def prompt(prompt_msg):
+    """
+    prompt asks the user the message that is input.
+    :param prompt_msg: message that the user is prompted with.
+    :return string: the users response to the prompt.
+    """
+    return bytes(input(prompt_msg), "utf-8")
 
-print(f"Received \"{data.decode('utf-8')}\"")
+def bytesToStr(bytes_data):
+    """
+    bytesToStr takes in a bytes variable and converts it to a string.
+    :param bytes_data: data in bytes.
+    :return string: the bytes variable as a string.
+    """
+    return bytes_data.decode('utf-8')
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        username = bytes(input("What is your username?\n"), "utf-8")
+        s.sendall(username)
+        while True:
+            msg = prompt("")
+            if not msg: break
+            s.sendall(msg)
+            data = s.recv(1024)
+            print(bytesToStr(data))
+
+if __name__ == "__main__":
+    main()
 
--- TCPChatServer/server.py
+++ TCPChatServer/server.py
@@ -1,26 +1,39 @@
 #Created 09/14/2023
 #Creator: Faa Diallo
-#This is a TCP server that receives a message and echos it back to
-#ths client. After that it closes the connection.
+#This is a TCP server which functions as a chat room. 
+#The client receives all of the messages that are sent to the server.
 import socket
 
 HOST = ""
 PORT = 65432
 
 def bytesToStr(bytes_data):
-    return bytes_data.decode('utf-8')
+    """
+    bytesToStr takes in a bytes variable and converts it to a string.
+    :param bytes_data: data in bytes.
+    :return string: the bytes variable as a string.
+    """
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    print(f"Login to the server on {PORT}.")
-    s.listen()
-    conn1, addr1 = s.accept()
-    print(f"Connected by {addr1}")
-    with conn1:
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
         s.listen()
-        conn2, addr2 = s.accept()
-        print(f"Connected by {addr2}")
+        print(f"Listening on port {PORT} for packets.")
+        print("-----Server Up------")
+        conn, addr = s.accept()
+        with conn:
+            #The first piece of data that's expected is the username.
+            username = conn.recv(1024)
+            print(f"User {bytesToStr(username)} has entered the chat room.")
         while True:
-            data1 = conn1.recv(1_000_000)
-            print(bytesToStr(data1))
+            data = conn.recv(1024)
+            if not data: 
+                print(f"User {bytesToStr(username)} has logged off.")
+                break
+            data = bytes(f"{bytesToStr(username)}: ", "utf-8") + data
+            conn.sendall(data)
+            print(bytesToStr(data))
+
+if __name__ == "__main__":
+    main()
 
